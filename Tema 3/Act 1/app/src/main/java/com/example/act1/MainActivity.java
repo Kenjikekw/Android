eda0ae981639244846ac9b7ad2519c97bb7ef08c
SAF <samuelarandafernandez6@gmail.com>
2023-10-04 15:47:25 +0200

    tema tres act1

---
+++ Tema 3/Act 1/app/src/main/java/com/example/act1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.act1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView greetingTextView = findViewById(R.id.greetingTextView);
+        greetingTextView.setText(R.string.hello_world);
+    }
+}
